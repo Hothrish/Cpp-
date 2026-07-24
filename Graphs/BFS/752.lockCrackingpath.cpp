9a0f10184b5bfcde1da150fd71dc7c8972550977
Hothrish <hothrish100@yahoo.com>
2026-07-24 18:53:57 +0530

    Update 752.lockCrackingpath.cpp

--- Graphs/BFS/752.lockCrackingpath.cpp
+++ Graphs/BFS/752.lockCrackingpath.cpp
@@ -54,3 +54,76 @@ public:
         return {}; // no path
     }
 };
+/*You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
+
+The lock initially starts at '0000', a string representing the state of the 4 wheels.
+
+You are given a list of deadends dead ends, meaning if the lock displays any of these codes, the wheels of the lock will stop turning and you will be unable to open it.
+
+Given a target representing the value of the wheels that will unlock the lock, return the minimum total number of turns required to open the lock, or -1 if it is impossible.
+
+ */
+class Solution {
+public:
+    int openLock(vector<string>& deadends, string target) {
+        if(target=="0000"){
+            return 0;
+        }
+        vector<int> state(10000);
+        vector<int> q1, q2;
+        for(auto& str:deadends){
+            state[stoi(str)]=-1;
+            if(stoi(str)==0){
+                return -1;
+            }
+        }
+        q1.push_back(0), q2.push_back(stoi(target));
+        state[0]=1, state[stoi(target)]=2;
+        int d=0;
+        while(!q1.empty() && !q2.empty()){
+            if(q1.size()>q2.size()){
+                swap(q1, q2);
+            }                
+            vector<int> tmpq;
+            for(int curr:q1){    
+                int st=state[curr];
+                int tmp=curr, base=1;
+                for(int i=0;i<4;i++){
+                    int up, down;
+                    if(tmp%10==9){
+                        up=curr-9*base;
+                    }
+                    else up=curr+base;
+                    if(tmp%10==0){
+                        down=curr+9*base;
+                    }
+                    else down=curr-base;
+                    if(state[up]!=-1){
+                        if(!state[up]){
+                            state[up]=st;
+                            tmpq.push_back(up);
+                        }
+                        else if(state[up]!=st) {
+                            return d+1;
+                        }
+                    }
+                    if(state[down]!=-1){
+                        if(!state[down]){
+                            state[down]=st;
+                            tmpq.push_back(down);
+                        }
+                        else if(state[down]!=st) {
+                            return d+1;
+                        }
+                    }
+                    tmp/=10;
+                    base*=10;
+                }
+            }
+            q1=tmpq;
+            d++;
+        }
+        return -1;
+    }
+};
+
